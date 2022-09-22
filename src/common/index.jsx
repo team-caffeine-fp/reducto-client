676e0d953e7e4fb72497d3313597bdbb6e40ff2e
agiskorska <skorskaga@gmail.com>
2022-09-22 15:58:44 +0100

    Add initial structure

---
+++ src/common/index.jsx
@@ -0,0 +1,4 @@
+export { default as Button } from './Button'
+export { default as Footer } from './Footer'
+export { default as NavBar } from './NavBar'
+export { default as Header } from './Header'
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import App from "./App.jsx"
+import React from 'react'
+import * as ReactDom from 'react-dom/client'
+
+const root = ReactDom.createRoot(document.getElementById('root'))
+root.render(<App />)
