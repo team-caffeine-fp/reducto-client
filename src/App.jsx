513471b60fc2be96994ea8adc4b860bb89f6cc69
madCakes <jason.pbrooks@hotmail.com>
2022-09-23 15:32:51 +0100

    add context/dataprovider

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import { Routes, Route} from 'react-router-dom'
 
 import { MainLayout } from "./layouts";
 import { Dashboard, Form } from "./pages";
+import { DataProvider } from "./context";
+
 
 export default function App(){
   return (
-    <Routes>
-      <Route path={'/'} element={<MainLayout />}>
-        <Route index element={<Dashboard />}></Route>
-        <Route path={'/form'} element={<Form />}></Route>
-      </Route>
-    </Routes>
+    <DataProvider>
+      <Routes>
+        <Route path={'/'} element={<MainLayout />}>
+          <Route index element={<Dashboard />}></Route>
+          <Route path={'/form'} element={<Form />}></Route>
+        </Route>
+      </Routes>
+    </DataProvider>
   );
 }
