4238e5fd3d034fabfdd3ca7b0cfdbfcc044eb02e
madCakes <jason.pbrooks@hotmail.com>
2022-09-26 09:24:05 +0100

    purge

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Routes, Route} from 'react-router-dom'
 
-import { MainLayout, Layout } from "./layouts";
+import { MainLayout} from "./layouts";
 import { Dashboard, Form } from "./pages";
 import { DataProvider } from "./context";
 
@@ -13,9 +13,6 @@ export default function App(){
         <Route path={'/'} element={<MainLayout />}>
           <Route index element={<Dashboard />}></Route>
           <Route path={'/form'} element={<Form />}></Route>
-        </Route>
-        <Route path={'/beans'} element={<Layout />}>
-        <Route path={'/beans'} element={<Dashboard />}></Route>
         </Route>
           
       </Routes>
