d4ba99cb94662a9f89a761325574863f0bc56acc
agiskorska <skorskaga@gmail.com>
2022-09-26 12:48:40 +0100

    Add form

--- src/common/Form/index.jsx
+++ src/common/Form/index.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import * as React from 'react';
+import { Box, Button, FormControl, FormHelperText, MenuItem, InputAdornment, InputLabel, OutlinedInput, Select } from '@mui/material'
 
-function index({data}) {
+export default function InputAdornments({data, category}) {
+    let obj = {}
+    for (let attr of data.fields) {
+        if(attr.options.length > 0) {
+            obj[attr.name] = attr.options[0]
+        } else {
+            obj[attr.name] = ''
+        }
+    }
+  const jsfiyString = (str) => {
+    return str.replace(' ', '_').toLowerCase()
+  }
+  const [values, setValues] = React.useState(obj)
+  const handleChange = e => {
+    const newInput = e.target.value;
+    setValues({...values, [e.target.name]: newInput});
+  }
+
+  const handleSubmit = () => {
+    const cat = jsfiyString(category)
+    console.log(values, cat)
+  }
+
   return (
-    <div>index</div>
-  )
+    <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+      <div>
+        {data.fields.map((f, i) => {
+            if(f.options.length == 0) {
+                return <FormControl key={i} sx={{ m: 1, width: '40%' }} variant="outlined">
+                <OutlinedInput
+                id={f.name}
+                aria-describedby={`${f.name}-text`}
+                endAdornment={f.units ? <InputAdornment position="start">{f.units[0]}</InputAdornment> : ''}
+                inputProps={{
+                    'aria-label': `${f.name}`,
+                }}
+                name={f.name}
+                onChange={handleChange}
+                value={values[f.name]}
+                />
+                <FormHelperText id={`${f.name}-text`}>{f.name}</FormHelperText>
+            </FormControl>
+            } else {
+                return <FormControl fullWidth key={i}>
+                    <InputLabel id="demo-simple-select-label">{f.name}</InputLabel>
+                    <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label={f.name}
+                    onChange={handleChange}
+                    name={f.name}
+                    value={values[f.name]}
+                    >
+                    {f.options.map((o, ind) => <MenuItem key={ind} value={o}>{o}</MenuItem>)}
+                    </Select>
+                </FormControl>
+            }
+        })}
+        <Button variant="contained" onClick={handleSubmit} style={{margin: '20px 0'}}>Submit</Button>
+      </div>
+    </Box>
+  );
 }
-
-export default index
--- src/pages/FormView/index.jsx
+++ src/pages/FormView/index.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material'
 
 import data from './data'
-
+import { Container, Form } from '../../common'
 function index() {
+
+  const [ category, setCategory ] = React.useState(data[0].category)
+  const [ currentData, setCurrentData ] = React.useState(data[0])
+  const handleOnChange = (e) => {
+    setCategory(e.target.value)
+  }
+  React.useEffect(() => {
+    const set = () => {
+      setCurrentData(...data.filter(d => d.category == category))
+    }
+    set()
+
+  }, [category])
   
   return (
-    <div>
-      {data[0].category}
-    </div>
+    <Container>
+      <FormControl fullWidth>
+        <InputLabel id="data-category">Category</InputLabel>
+        <Select
+          labelId="data-category"
+          id="demo-simple-select"
+          label='data-category'
+          onChange={handleOnChange}
+          value={category}
+        >
+        {data.map((d, ind) => <MenuItem key={ind} value={d.category}>{d.category}</MenuItem>)}
+        </Select>
+        <Form data={currentData} category={category} />
+      </FormControl>
+    </Container>
   )
 }
 
