92f04f867484c2a5c65d910dd530d9c35ee61b7a
agiskorska <skorskaga@gmail.com>
2022-09-26 08:34:32 +0100

    Fix warnings

--- src/common/SideBar/index.jsx
+++ src/common/SideBar/index.jsx
@@ -84,8 +84,8 @@ export default function SideBar() {
       >
         <DrawerHeader >
           <IconButton onClick={handleDrawerClose} data-testid="IconButton">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fillRule="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
             </svg>
           </IconButton>
         </DrawerHeader> 
