561e1bdfc4c0d56a82374cb6fc4202237ce5c5df
Ioana Gheorghias <ioana.gh27@gmail.com>
2022-09-26 17:23:35 +0100

    Change context

--- src/context.jsx
+++ src/context.jsx
@@ -14,12 +14,12 @@ export const DataProvider = ({ children }) => {
 
   //user
   const [user, setUser] = useState(null);
+  const [userId, setUserId] =useState(null)
 
   async function register(username, businessname, email, password) {
     try {
         const resp = await axios.post("/register", { username, businessname, email, password });
         localStorage.setItem("token", resp.data.token)
-        setUser(resp.data.user)
         navigate('/login');
     } catch (error) {
         console.log(error)
@@ -28,20 +28,58 @@ export const DataProvider = ({ children }) => {
 
   async function login(username, password) {
     try {
-        const resp = await axios.post("/auth/login", { username, password });
+      // let resp = await axios.post("/auth/login", { username, password });
+      // const {token, _id} = resp.data
+      // localStorage.setItem("token", token)
+      // resp = await axios.get(`/users/${_id}`, {
+      //   headers: {
+      //     'Authorization': `Bearer ${token}`
+      //   }
+      // })
+      // setUser(resp.data);
+      // navigate('/');
+      const options = {
+        headers: new Headers({
+            'Authorization': localStorage.getItem("token")
+        }) 
+    }
+        const resp = await axios.post("/auth/login", { username, password }, options);
         localStorage.setItem("token", resp.data.token)
-        setUser(resp.data.user);      
+        const id = resp.data._id
+        const user = await axios.get(`/user/${id}`);
+        setUser(user)  
+        setUserId(id)  
         navigate('/');
     } catch (error) {
         console.log(error)
     }
   }
 
-  function logout() {
-    localStorage.removeItem('token')
-    setUser(null);
-    navigate("/login");
+  async function logout() {
+    try{
+//       const token = localStorage.getItem('token')
+//       await axios.post('/auth/logout', {}, {headers: {
+//         'Authorization': `Bearer ${token}`
+// }})
+//       localStorage.removeItem('token')
+//       setUser(null);    
+//       navigate("/login");
+      const options = {
+        headers: new Headers({
+            'Authorization': localStorage.getItem("token")
+        }) 
+    }
+      const resp = await axios.post("/auth/logout",{_id: userId}, options);
+      console.log(resp)
+      localStorage.removeItem('token')
+      setUser(null);
+      setUserId(null)
+      navigate("/login");
+    } catch (error) {
+      console.log(error)
+  }
  }
+
  
 
   return (
