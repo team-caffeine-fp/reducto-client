817db58ef0ccd47e31f64f03f7f91bf5ad388ded
agiskorska <skorskaga@gmail.com>
2022-09-27 07:18:23 +0100

    Chage chart functions to show right amount of data

--- src/utils.js
+++ src/utils.js
@@ -1,65 +1,73 @@
 import axios from 'axios'
 
-
-//TODO loop through the amount of data and create additional colors 
 export const createPieChartObject = (labels, title, data) => {
-    return {
-        type: 'pie',
-        data: {labels: labels,
-          datasets: [{
-            label: title,
-            data: data,
-            backgroundColor: [
-              'rgb(255, 99, 132)',
-              'rgb(54, 162, 235)',
-              'rgb(20, 99, 132)',
-              'rgb(54, 162, 50)',
-              'rgb(255, 205, 86)'
-            ],
-            hoverOffset: 4
-          }],}
+  const backgroundColors = [
+    'rgb(255, 99, 132)',
+    'rgb(54, 162, 235)',
+    'rgb(20, 99, 132)',
+    'rgb(54, 162, 50)',
+    'rgb(20, 99, 11)',
+    'rgb(54, 55, 50)',
+    'rgb(255, 205, 86)'
+  ]
+  return {
+    type: 'pie',
+    data: {labels: labels,
+      datasets: [{
+        label: title,
+        data: data,
+        backgroundColor: backgroundColors.slice(0, labels.length),
+        hoverOffset: 4
+      }],
     }
+  }
 }
 
 export const createBarChartObject = (labels, title, data) => {
-    const barData = {
-        labels: data,
-        datasets: [{
-          label: title,
-          data: data,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 205, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(201, 203, 207, 0.2)'
-          ],
-          borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
-            'rgb(75, 192, 192)',
-            'rgb(54, 162, 235)',
-            'rgb(153, 102, 255)',
-            'rgb(201, 203, 207)'
-          ],
-          borderWidth: 1
-        }]
+  const backgroundColors = [
+    'rgba(255, 99, 132, 0.8)',
+    'rgba(255, 159, 64, 0.8)',
+    'rgba(255, 205, 86, 0.8)',
+    'rgba(75, 192, 192, 0.8)',
+    'rgba(54, 162, 235, 0.8)',
+    'rgba(153, 102, 255, 0.8)',
+    'rgba(54, 255, 235, 0.8)',
+    'rgba(153, 12, 255, 0.8)',
+    'rgba(201, 203, 207, 0.8)'
+  ]
+  const borderColors = [
+    'rgb(255, 99, 132)',
+    'rgb(255, 159, 64)',
+    'rgb(255, 205, 86)',
+    'rgb(75, 192, 192)',
+    'rgb(54, 162, 235)',
+    'rgb(153, 102, 255)',
+    'rgb(54, 162, 12)',
+    'rgb(153, 102, 45)',
+    'rgb(201, 203, 207)'
+  ]
+  const barData = {
+    labels: labels,
+    datasets: [{
+      label: title,
+      data: data,
+      backgroundColor: backgroundColors.slice(0, labels.length),
+      borderColor: borderColors.slice(0, labels.length),
+      borderWidth: 1
+    }]
+  }
+  return {
+    type: 'bar',
+    data: barData,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
       }
-    return {
-        type: 'bar',
-        data: barData,
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        },
-      }
-    }
+    },
+  }
+}
 
 export const fetchDataFromApi = async (data, cat, setData) => {
   console.log(cat)
