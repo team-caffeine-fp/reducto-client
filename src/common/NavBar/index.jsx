3f05b02f90963398aa0855027fdb8a0aa8bb57b9
madCakes <jason.pbrooks@hotmail.com>
2022-09-27 20:57:23 +0100

    add months

--- src/common/NavBar/index.jsx
+++ src/common/NavBar/index.jsx
@@ -1,74 +1,73 @@
-import * as React from 'react';
-import { Outlet } from 'react-router-dom'
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import { NavLink } from 'react-router-dom'
-import { AiOutlineMenu } from "react-icons/ai"; 
-import { HiChevronLeft } from "react-icons/hi"; 
+import * as React from "react";
+import { Outlet } from "react-router-dom";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { NavLink } from "react-router-dom";
+import { AiOutlineMenu } from "react-icons/ai";
+import { HiChevronLeft } from "react-icons/hi";
 
-import { Outlet } from 'react-router-dom'
-import useWindowDimensions from '../WindowDimensions';
+import useWindowDimensions from "../WindowDimensions";
 
-import { useData } from '../../context';
+import { useData } from "../../context";
 
 const drawerWidth = 240;
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
   ({ theme, open }) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginLeft: `-${drawerWidth}px`,
     ...(open && {
-      transition: theme.transitions.create('margin', {
+      transition: theme.transitions.create("margin", {
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen,
       }),
       marginLeft: 0,
     }),
-  }),
+  })
 );
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
+  transition: theme.transitions.create(["margin", "width"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
+  justifyContent: "flex-end",
 }));
 
 export default function PersistentDrawerLeft() {
@@ -76,8 +75,7 @@ export default function PersistentDrawerLeft() {
   const [open, setOpen] = React.useState(false);
 
   const { height, width } = useWindowDimensions();
-  const {loadData} = useData()
-
+  const { userData } = useData();
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -89,32 +87,31 @@ export default function PersistentDrawerLeft() {
 
   React.useEffect(() => {
     if (width > 900) {
-      setOpen(true)
+      setOpen(true);
     }
-  }, [width])
-  
+  }, [width]);
+
+  console.log("💥", userData);
+  console.log();
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
-        <Toolbar >
+        <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
           >
             <AiOutlineMenu />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 0.05 }}>
-            <NavLink to={'/'}>Home Page</NavLink>
+            <NavLink to={"/"}>Home Page</NavLink>
           </Typography>
           <Typography variant="h6" component="div" sx={{ flexGrow: 0.05 }}>
-            <NavLink to={'/form'}>Form</NavLink>
-          </Typography>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Some other stuff
+            <NavLink to={"/form"}>Form</NavLink>
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
@@ -123,32 +120,44 @@ export default function PersistentDrawerLeft() {
         sx={{
           width: drawerWidth,
           flexShrink: 0,
-          '& .MuiDrawer-paper': {
+          "& .MuiDrawer-paper": {
             width: drawerWidth,
-            boxSizing: 'border-box',
+            boxSizing: "border-box",
           },
         }}
         variant="persistent"
         anchor="left"
         open={open}
         PaperProps={{
-            sx: {
-              backgroundColor: "#1976d2"
-            }
-          }}
+          sx: {
+            backgroundColor: "#1976d2",
+          },
+        }}
       >
-        
-        <DrawerHeader >
+        <DrawerHeader>
           <IconButton onClick={handleDrawerClose}>
-            <HiChevronLeft style={{ fill: '#ffffff'}}/>
+            <HiChevronLeft style={{ fill: "#ffffff" }} />
           </IconButton>
         </DrawerHeader>
         <Divider />
         <List>
-          {['News', `Your emissions`, 'Stuff'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton sx={{color:'#ffffff'}}>
-               <ListItemText primary={text} />
+          {[
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+          ].map((month, index) => (
+            <ListItem key={index} disablePadding>
+              <ListItemButton sx={{ color: "#ffffff" }}>
+                <ListItemText primary={month} />
               </ListItemButton>
             </ListItem>
           ))}
