4b5268656980c4b15a7b78c0404ff2a78a615bcf
agiskorska <skorskaga@gmail.com>
2022-09-27 15:51:08 +0100

    Add changes to context and merge with dev

--- src/common/MainGrid/index.jsx
+++ src/common/MainGrid/index.jsx
@@ -8,19 +8,23 @@ import Typography from '@mui/material/Typography'
 import { Chart, Container } from '../'
 import styles from './index.module.css'
 import { createPieChartObject, createBarChartObject, createDataStructureForCharts } from '../../utils'
-import { userId, herokuUrl } from '../../settings';
+import { herokuUrl } from '../../settings';
+import { useData } from '../../context'
+
 function index() {
-  const [ userData, setUserData] = React.useState({})
   const [ barConfig, setBarConfig ] = React.useState({})
   const [ pieConfig, setPieConfig ] = React.useState({})
-  const [ id , setId ] = React.useState(0)
+  const [ chartId , setChartId ] = React.useState(0)
+  const { setUserData, userId } = useData()
+
   React.useEffect(() => {
-    const fetchData = async () => {
+    const fetchData = async (setBarConfig, setPieConfig, setChartId) => {
       const data = await axios.get(herokuUrl + '/users/' + userId)
-        setId(prev => prev + 1)
+        setChartId(prev => prev + 1)
+        setUserData(data.data)
         createDataStructureForCharts(data.data, setBarConfig, setPieConfig)
-    }   
-    fetchData()
+    }
+    fetchData(setBarConfig, setPieConfig, setChartId)
     
   }, [])
 
@@ -29,8 +33,8 @@ function index() {
     <Container variant={'mainGrid'}>
         <Grid container spacing={2} style={{width: '100%'}}>
           <Grid item xs={8}>
-              <Chart config={barConfig} canvasId={id} />
-              <Chart config={pieConfig} canvasId={id}/>
+              <Chart config={barConfig} canvasId={chartId} />
+              <Chart config={pieConfig} canvasId={chartId}/>
           </Grid>
           <Grid item xs={4} style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}} >
               <Card style={{flexBasis: '30%', borderRadius: '20px', boxShadow: '2px 2px 2px 1px rgba(0, 0, 0, 0.2)', backgroundColor: "red", width: '100%', padding: '50px', marginBottom: '30px' }}> 
