0e873c2976aee0176fd0d0065b11d7896bb1398f
madCakes <jason.pbrooks@hotmail.com>
2022-09-27 17:32:17 +0100

    purge

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,15 +6,6 @@ import { MainLayout } from "../layouts";
 export default function ProtectedRoute({ redirectTo }) {
     const { fetchOnReload, user } = useData()
 
-    React.useEffect(() => {
-        window.addEventListener("beforeunload", fetchOnReload)
-        return () => {
-          window.removeEventListener("beforeunload", fetchOnReload)
-        };
-      }, []);
-    console.log(user)
-
-    return user ? <MainLayout /> : <Navigate to={redirectTo} />
-    
+    return user ? <MainLayout /> : <Navigate to={redirectTo} />    
     
 }
