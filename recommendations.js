fd7183a8b99a4e1c71b1698b72fc0a6705421884
Ioana Gheorghias <ioana.gh27@gmail.com>
2022-09-27 22:47:24 +0100

    Add recommendations file

---
+++ recommendations.js
@@ -0,0 +1,50 @@
+{
+    recommendations: [
+        {
+            category: {
+                "travel_flights": {
+                    1: "Pack light - The more you pack, the heavier the aircraft — and the heavier the aircraft, the more fuel required. Now, do you really need that extra things 'just in case'? Rent bulky gear at your destination instead of packing it, use miniature refillable containers for toiletries and pack versatile items that can be reworn.",
+                    2: "Choose economy - All of that extra stretch room in business class does come at a substantial premium for the environment. Business and first-class seating take up more space and weight, so more fuel is required per passenger (first-class tickets account for, on average, four times the emissions of an economy seat) and, ultimately, the number of flights increases.",
+                    3: "Choose - Shorter flights have a greater carbon footprint. Bypassing flying when there are convenient routes by coach, train or car — particularly if driving a hybrid or travelling with other passengers — will reduce your environmental impact.",
+                    "link": "https://www.nationalgeographic.co.uk/travel/2020/03/10-ways-you-can-reduce-carbon-emissions-when-you-travel"
+                },
+                "freight_flights": {
+                    1: "Carbon neutrality -Carbon offsetting schemes allow companies or individuals to counteract their carbon footprint by investing in environmental schemes or projects around the world. This could be anything from tree planting to developing clean energy schemes.",
+                    2: "Flying more efficient aircraft - Using better technology to set more efficient flight paths and cut delays",
+                    3: "Switching to lower-carbon fuels and investing in emissions-offsetting initiatives either within or outside of the aviation industry. By implementing one or more of these measures, airlines all over the world can meet the CO2 cap set by the CORSIA policy. ",
+                    "link": "https://www.ies.co.uk/news/can-air-freight-go-carbon-neutral"
+                },
+                "road_freight": {
+                    1: "Pick the least-polluting, most efficient vehicle that meets your needs - Just switching from a vehicle that gets 20-mpg to a vehicle that gets 25-mpg car reduces your greenhouse gas emissions by 1.7 tons annually.",
+                    2: "Electric vehicles (EVs) offer a low-carbon alternative to gasoline-powered vehicles. An EV can reduce your carbon footprint and save you money over the lifetime of the vehicle.",
+                    3: "Eco-driving - Optimal driving techniques can also help you cut emissions and save money in a gasoline-powered car. Hard acceleration and braking can waste fuel and lower your mileage. Studies show that for every 5 miles per hour you drive over 60 miles per hour, fuel economy is lowered by 7 percent, so go easy on the brakes and gas pedal. You can also lower impacts by reducing time spent idling, and using overdrive and cruise control. And because a properly-maintained vehicle can improve your gas mileage and fuel economy by 4 percent, remember to have your vehicle tuned up, tires inflated, and oil and air filter cleaned out regularly.",
+                    "link": "https://www.c2es.org/content/reducing-your-transportation-footprint/"
+                },
+                "cloud_computing": {
+                    1: "Hyperscalers and colocated data centres are more efficient, driven by better energy utilisation, more efficient cooling systems and increased workloads per server.",
+                    2: "Innovative energy-efficiency options ",
+                    3: "Understand your current carbon footprint and identify opportunities to reduce that footprint"
+
+                },
+                "cloud_storage": {
+                    1: "Understand your current carbon footprint and identify opportunities to reduce that footprint",
+                    2: "Choosing cleaner cloud regions to run workloads is one of the simplest and most effective ways to reduce carbon emissions.",
+                    3: "Cost optimization practices that reduce idle (or inefficient use of) cloud resources also translate well to carbon footprint reduction",
+                    "link": "https://cloud.google.com/architecture/reduce-carbon-footprint"
+                },
+                "cloud_memory": {
+                    1: "Vertical scaling - If demand for a particular application or service falls, scaling down the server supporting it can free up unneeded resources.",
+                    2: "Rearchitecting applications to be “cloud-native” delivers considerable returns in both cost savings and carbon footprint reduction. By using containers and “serverless” technologies, you maximise resharing of resources, eliminate wasted server time and thus minimise the carbon footprint of your applications.",
+                    3: "Planning your cloud usage in advance can also reduce emissions.",
+                    "link": "https://techmonitor.ai/leadership/sustainability/how-to-cut-carbon-footprint-your-cloud-usage"
+                },
+                "electricity": {
+                    1: "Low energy lighting - A modern LED bulb uses 80-90% less energy than a traditional lightbulb, which helps to lower your carbon emissions and electricity bills. If you replace all the bulbs in your home with LED lights, you could reduce your carbon dioxide emissions by up to 65kg a year.",
+                    2: "Energy efficient appliances - Choosing energy efficient appliances is a straightforward way to reduce your carbon footprint. If you are looking to replace an appliance, choosing a model with a high energy efficiency rating that meets your needs and budget will minimise carbon emissions.",
+                    3: "Reduce, reuse, recycle - Every product we buy has a carbon footprint: the energy used in making that product and getting it to you will almost certainly have caused some carbon emissions. You should try to re-use wherever possible and recycle when you no longer need something",
+                    "link": "https://energysavingtrust.org.uk/top-tips-to-reduce-your-carbon-emissions/"
+                }
+            }
+        }
+    ]
+}
